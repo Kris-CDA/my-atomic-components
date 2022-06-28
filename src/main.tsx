d9cf6cacc45a326fd846d36a3b0ab30271a727e6
Christophe Daniel <cdaniel@easyvista.com>
2022-06-28 21:25:57 +0200

    remove strict mode because it fails on combobox

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,4 @@ import App from './App';
 
 const container: Element | DocumentFragment = document.getElementById('root')!;
 
-ReactDOM.createRoot(container).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(container).render(<App />);
